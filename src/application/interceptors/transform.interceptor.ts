e38a74f9235cad33690b104b2057b02441084270
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-24 15:44:02 -0300

    feat: create the error filter / transform interceptor

---
+++ src/application/interceptors/transform.interceptor.ts
@@ -0,0 +1,10 @@
+import { NestInterceptor, Injectable, CallHandler } from '@nestjs/common';
+import { instanceToPlain } from 'class-transformer';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(_: any, next: CallHandler<any>) {
+    return next.handle().pipe(map((data) => instanceToPlain(data)));
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './application/interceptors/transform.interceptor';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { BASE_PATH } from './application/constants/constants';
+import { GeneralErrorFilter } from './application/filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('Main');
+
+  app.useGlobalFilters(new GeneralErrorFilter(logger));
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.setGlobalPrefix(BASE_PATH);
+
   await app.listen(3000);
 }
 bootstrap();
