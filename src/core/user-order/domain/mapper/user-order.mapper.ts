aa2eb38002b7a405a4a553860a501dfa6f56ca58
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-25 11:12:40 -0300

    feat: implements the mapper interface to user-order domain entity

---
+++ src/core/user-order/domain/mapper/user-order.mapper.ts
@@ -0,0 +1,55 @@
+import { IMapper } from '../../../../application/patterns/mapper.interface';
+import { Order } from '../entities/order';
+import { UserOrder } from '../entities/user-order';
+import { OrderInput } from '../entities/input/order-input';
+
+export class UserOrderMapper implements IMapper<OrderInput[], UserOrder[]> {
+  mapTo(valueToMap: OrderInput[]): UserOrder[] {
+    const valueGroupedByOrder = this.groupDataByUserAndOrder(valueToMap);
+    const transformedData: UserOrder[] = Array.from(
+      valueGroupedByOrder.values(),
+    ).map((user) => ({
+      userId: user.userId,
+      name: user.name,
+      orders: Array.from(user.order.values()),
+    }));
+
+    return transformedData;
+  }
+
+  public groupDataByUserAndOrder(orderInput: OrderInput[]) {
+    return orderInput.reduce((mapSet, current) => {
+      const { userId, name, orderId, productId, value, date } = current;
+
+      if (!mapSet.has(userId)) {
+        mapSet.set(userId, {
+          userId: userId,
+          name: name,
+          order: new Map<number, Order>(),
+        });
+      }
+
+      const user = mapSet.get(userId)!;
+
+      if (!user.order.has(orderId)) {
+        user.order.set(orderId, {
+          orderId,
+          date: date,
+          total: 0,
+          products: [],
+        });
+      }
+
+      const order = user.order.get(orderId)!;
+
+      order.products.push({
+        productId,
+        value: value,
+      });
+
+      order.total += value;
+
+      return mapSet;
+    }, new Map<number, { userId: number; name: string; order: Map<number, Order> }>());
+  }
+}
