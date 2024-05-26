f02cabf2fe3b946338677955fe2079b5b4807636
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-26 20:26:49 -0300

    feat: create a process util function to read lines  and parse data

--- src/application/utils/utils.ts
+++ src/application/utils/utils.ts
@@ -1,4 +1,5 @@
 import { HttpStatus } from '@nestjs/common';
+import { Interface } from 'readline';
 import { ErrorType } from '../types/error-types.enum';
 
 export const getHttpStatusCodeForErrorType = (errorCode: ErrorType): number => {
@@ -7,7 +8,60 @@ export const getHttpStatusCodeForErrorType = (errorCode: ErrorType): number => {
     [ErrorType.DATABASE_ERROR]: HttpStatus.INTERNAL_SERVER_ERROR,
     [ErrorType.SYNC_SERVICE_ERROR]: HttpStatus.BAD_REQUEST,
     [ErrorType.BAD_REQUEST_ERROR]: HttpStatus.BAD_REQUEST,
+    [ErrorType.UNPROCESSABLE_ENTITY]: HttpStatus.UNPROCESSABLE_ENTITY,
   };
 
   return errors[errorCode];
+};
+
+export type Callback = {
+  execute: (params: any) => any;
+};
+
+export const processLinesByChunk = async <TypeResponse>(
+  lines: Interface,
+  size: number,
+  callback: Callback,
+): Promise<TypeResponse> => {
+  const LINE_SIZE = 95;
+  const chunk = [];
+  const index = 1;
+  const linesWithError = [];
+
+  for await (const line of lines) {
+    if (line.length !== LINE_SIZE) {
+      linesWithError.push({ line, lineSize: index });
+      continue;
+    }
+    const order = Object.assign(
+      {},
+      {
+        user_id: parseInt(line.substring(0, 10).trim()),
+        name: line.substring(10, 55).trim(),
+        order_id: parseInt(line.substring(55, 65).trim()),
+        product_id: parseInt(line.substring(65, 75).trim()),
+        value: parseFloat(line.substring(75, 87)),
+        date: parseDate(line.substring(87, 95).trim()),
+      },
+    );
+    chunk.push(order);
+
+    if (chunk.length === size) {
+      await callback.execute(chunk);
+      chunk.splice(0, chunk.length);
+    }
+  }
+
+  if (chunk.length) {
+    await callback.execute(chunk);
+  }
+
+  return linesWithError as TypeResponse;
+};
+
+export const parseDate = (date: string): Date => {
+  const year = parseInt(date.slice(0, 4));
+  const month = parseInt(date.slice(4, 6));
+  const day = parseInt(date.slice(6, 8));
+  return new Date(year, month, day);
 };
