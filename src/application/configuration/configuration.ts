d0f6f2beac27745f0e6ec287cdebd8c7bbde97f3
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-26 20:24:42 -0300

    feat: add new environment values and update type

--- src/application/configuration/configuration.ts
+++ src/application/configuration/configuration.ts
@@ -5,4 +5,6 @@ export const getConfiguration = (): ConfigurationType => ({
   DATABASE_URI: process.env.DATABASE_URI,
   APPLICATION_TITLE: process.env.APPLICATION_TITLE,
   APPLICATION_VERSION: process.env.APPLICATION_VERSION || '1',
+  MAX_CHUNK_SIZE: parseInt(process.env.MAX_CHUNK_SIZE, 10) || 2048,
+  ALLOW_REPEAT_FILE_FLAG: parseInt(process.env.ALLOW_REPEAT_FILE_FLAG, 10) || 0,
 });
--- src/application/configuration/type/configuration.type.ts
+++ src/application/configuration/type/configuration.type.ts
@@ -3,4 +3,6 @@ export type ConfigurationType = {
   DATABASE_URI: string;
   APPLICATION_TITLE: string;
   APPLICATION_VERSION: string;
+  ALLOW_REPEAT_FILE_FLAG: number;
+  MAX_CHUNK_SIZE: number;
 };
