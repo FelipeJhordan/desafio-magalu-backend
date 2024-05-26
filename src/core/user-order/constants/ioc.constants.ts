671c67a85eb30bbe78d1d9b61ba01549f0b84010
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-26 20:27:20 -0300

    feat: set ioc tokens to use in nest modules

---
+++ src/core/user-order/constants/ioc.constants.ts
@@ -0,0 +1,2 @@
+export const I_MAPPER_TOKEN = 'IMapperToken';
+export const I_USER_ORDER_REPOSITORY_TOKEN = 'IUserOrderRepositoryToken';
---
+++ src/file-hash/constants/ioc/ioc.constants.ts
@@ -0,0 +1,2 @@
+export const I_FILE_HASH_REPOSITORY_TOKEN = 'IFileHashRepositoryToken';
+export const I_HASH_ADAPTER_TOKEN = 'IHashAdapterToken';
