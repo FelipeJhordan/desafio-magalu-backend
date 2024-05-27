558d98fe4b120e2d8ed3cc6474e2164c63d44d4c
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-26 22:23:06 -0300

    refactor: remove needless error types

--- src/application/utils/utils.ts
+++ src/application/utils/utils.ts
@@ -4,9 +4,6 @@ import { ErrorType } from '../types/error-types.enum';
 
 export const getHttpStatusCodeForErrorType = (errorCode: ErrorType): number => {
   const errors = {
-    [ErrorType.NOT_FOUND_ERROR]: HttpStatus.NOT_FOUND,
-    [ErrorType.DATABASE_ERROR]: HttpStatus.INTERNAL_SERVER_ERROR,
-    [ErrorType.SYNC_SERVICE_ERROR]: HttpStatus.BAD_REQUEST,
     [ErrorType.BAD_REQUEST_ERROR]: HttpStatus.BAD_REQUEST,
     [ErrorType.UNPROCESSABLE_ENTITY]: HttpStatus.UNPROCESSABLE_ENTITY,
   };
